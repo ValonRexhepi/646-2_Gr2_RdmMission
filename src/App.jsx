515c34ea1201f284f6bfdcbf1e6614a3f9a18a93
Vincent Le <vincentle.fh@gmail.com>
2022-03-11 11:11:11 +0200

    :card_index_dividers::test_tube: Initialized db, tested db connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./styles/app.css"
+import { db } from "./services/firebase"
+import { onValue, ref } from "firebase/database"
 
 function App() {
+    // Testing database connection
+    useEffect(() => {
+        onValue(ref(db), snapshot => {
+            const data = snapshot.val()
+            if (data !== null) {
+                console.log(data)
+            } else {
+                console.log("There's no data")
+            }
+        })
+    }, [])
+
     return (
         <div>
             <h1>Hello World</h1>
---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "firebase/database"
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
+
+const app = initializeApp(firebaseConfig)
+
+// Use realtime db as db outside this module
+export const db = getDatabase(app)
