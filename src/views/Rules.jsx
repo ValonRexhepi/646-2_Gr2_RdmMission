9544e5bd2d1d98469c9a76a931cfbf943236eb0c
ssharma178 <bgk150@myy.haaga-helia.fi>
2022-03-17 22:07:13 +0200

    having better, uniform looking screens

--- src/views/Rules.jsx
+++ src/views/Rules.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Page, Document , pdfjs } from "react-pdf";
 import Mission_GDR_2022_Regles from "../docs/Mission_GDR_2022_Regles.pdf";
-import "../styles/app.css"
+import { Link } from "react-router-dom";
+import styles from "../styles/app.module.css"
 
 export default function Rules() {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
     return(
         <div>
-            <Document file={Mission_GDR_2022_Regles}>
-                <Page pageNumber={1} />
-                <Page pageNumber={2} />
-            </Document>
+            <Link to ="/" className={styles.button}>Go Home</Link>
+            <div className={styles.pdfdiv}>
+                <Document file={Mission_GDR_2022_Regles}>
+                    <Page pageNumber={1} />
+                    <Page pageNumber={2} />
+                </Document>
+            </div>
         </div>
     )
 }
