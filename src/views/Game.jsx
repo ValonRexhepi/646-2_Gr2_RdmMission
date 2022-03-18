4267570f4b3bfb87484baf0d309fffff5d403d04
Bastien Ferrari <bastien.ferrari@hes-so.ch>
2022-03-18 11:10:53 +0100

    Creation Roadmap + Form of Roadmap

--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -17,6 +17,10 @@ export default function Game() {
         <div>
             <h2>Game</h2>
             <p>Session Id: {session.id}</p>
+            <Link to={{
+                pathname:"/roadmap",
+                state: session.id
+            }}>Roadmap</Link>
             <Link to="/">
                 <button onClick={endGame}>End Game</button>
             </Link>
