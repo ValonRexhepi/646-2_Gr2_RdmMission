43ece5e7c21fa5f0906b8e9bd05494091a768913
Vincent Le <vincentle.fh@gmail.com>
2022-04-03 15:55:11 +0300

    Finished showing cards, cards are draggable

--- src/components/CardContainer/Card.jsx
+++ src/components/CardContainer/Card.jsx
@@ -1,22 +1,37 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
+import Draggable from "react-draggable"
 
-export default function Card({ index, source }) {
+export default function Card({ source }) {
+    const [ position, setPosition ] = useState({ x: 0, y: 0 })
+
+    const cardRef = useRef(null)
+
     const cardPosition = useRef({
         position: "absolute",
         top: Math.floor(Math.random() * 50 ),
     });
 
+    const handleDrag = (_, ui) => {
+        cardRef.current.classList.add("moving")
+        setPosition({ x: position.x + ui.deltaX, y: position.y + ui.deltaY })
+    }
+
     return (
-        <div 
-            className={`card card-${index}`} 
-            style={cardPosition.current}
+        <Draggable
+            nodeRef={cardRef}
+            onDrag={handleDrag}
         >
-            <img 
-                src={source} 
-                alt="Card" 
-                className={`card-img card-img-${index}`}
-                width={660}
-            />
-        </div>
+            <div 
+                ref={cardRef}
+                className="card"
+                style={cardPosition.current}
+            >
+                <img 
+                    src={source} 
+                    alt="Card" 
+                    width={640}
+                />
+            </div>
+        </Draggable>
     )
 }
--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { storeDataLS, getDataLS } from "../../utils/helpers"
 
 import Card from "./Card"
 
@@ -6,28 +7,31 @@ export default function CardContainer({ numberToBeFound }) {
     const [ imgSrc, setImgSrc ] = useState([])
 
     useEffect(() => {
+        const cardImgs = getDataLS("cards")
+        setImgSrc(cardImgs)
+        console.log(cardImgs)
+    }, [])
+
+    useEffect(() => {
         if (numberToBeFound) {
             try {
                 const src = require(`../../assets/cards/card${numberToBeFound}.jpg`)
                 setImgSrc([...new Set([ ...imgSrc, src ])])
+                storeDataLS("cards", [...new Set([ ...imgSrc, src ])])
             } catch {
                 setImgSrc([...new Set([ ...imgSrc ])])
+                storeDataLS("cards", [...new Set([ ...imgSrc ])])
                 return
             }
-        } else {
-            setImgSrc([...new Set([ ...imgSrc ])])
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [numberToBeFound])
-
 
     return (
         <>
             {
                 imgSrc 
-                ? imgSrc.map((source, i) => (
-                    <Card key={i} index={i} source={source} />
-                ))
+                ? imgSrc.map((source, i) => <Card key={i} source={source} />)
                 : null
             }
         </>
